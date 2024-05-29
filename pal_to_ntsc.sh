8d9db83d7a39eeaaf4befc086feaf311ddda04d0
Peter Inzani <inzani.p@gmail.com>
2024-05-28 22:09:25 -0300

    fixed integers

--- pal_to_ntsc.sh
+++ pal_to_ntsc.sh
@@ -46,18 +46,15 @@ do
     # echo "File $FN"
     # echo "File base $FN_BASE"
     # echo "$OUTDIR/${FN_BASE}_final.mkv"
-    # ffmpeg -i $F -af "asetrate=48000" -vf "fps=fps=ntsc_film" -aspect 16:9 -acodec ac3 -vcodec libx264 -preset slow -crf 18 "$OUTDIR/$FN" > ${OUTDIR}/${FN_BASE}_ff_out.txt 2> ${OUTDIR}/${FN_BASE}_ff_err.txt
-    # -map:0:v:0 -map:0:a:0
-    # 2^x/12
-    # ffmpeg -y -i $F -vf "fps=fps=ntsc_film" -r 24000/1001 -vsync "cfr" -af "rubberband=pitch=24000/1001/25, rubberband=tempo=24000/1001/25" -aspect 4:3 -acodec ac3 -vcodec hevc_videotoolbox -q:v 80 -tag:v hvc1 "$OUTDIR/$FN" > ${OUTDIR}/${FN_BASE}_ff_out.txt 2> ${OUTDIR}/${FN_BASE}_ff_err.txt
-    # ffmpeg -y -i $F -vf "setpts=25/24000/1001*PTS" -r 24000/1001 -vsync "cfr" -af "rubberband=pitch=24000/1001/25, rubberband=tempo=24000/1001/25" -aspect 4:3 -acodec ac3 -vcodec hevc_videotoolbox -q:v 80 -tag:v hvc1 "$OUTDIR/$FN" > ${OUTDIR}/${FN_BASE}_ff_out.txt 2> ${OUTDIR}/${FN_BASE}_ff_err.txt
     echo "Resampling audio and video"
     samplerate=$(ffprobe -v error -select_streams a:0 -show_entries stream=sample_rate -of default=noprint_wrappers=1:nokey=1 $F)
-    factor=24000/25025
+    let factor=24000.0/25025.0
+    let inverse_factor=1.0/$factor
+    let rate=24000.0/1001.0
     ffmpeg -threads auto -hwaccel auto -y -i $F -vf "setpts=PTS*$inverse_factor, fps=fps=ntsc_film" -af "asetrate=$factor*$samplerate" -ar $samplerate -aspect 4:3 -c:v hevc_videotoolbox -q:v 80 -c:a aac -b:a 320k -profile:v main -tag:v hvc1 "$OUTDIR/$FN_RESAMPLED" > ${LOGDIR}/${FN_BASE}_ff_out.txt 2> ${LOGDIR}/${FN_BASE}_ff_err.txtecho "extracting chapters"
     mkvextract "$F" chapters "$OUTDIR/$FN_CHAPTERS"
     echo "Merging chapters with resampled"
-    mkvmerge -o "$OUTDIR/$FN_FINAL" --chapter-sync "0,25025/24000" --chapters "$OUTDIR/$FN_CHAPTERS" "$OUTDIR/$FN_RESAMPLED" > ${LOGDIR}/${FN_BASE}_merge_out.txt 2> ${LOGDIR}/${FN_BASE}_merge_err.txt
+    mkvmerge -o "$OUTDIR/$FN_FINAL" --chapter-sync "0,$inverse_factor" --chapters "$OUTDIR/$FN_CHAPTERS" "$OUTDIR/$FN_RESAMPLED" > ${LOGDIR}/${FN_BASE}_merge_out.txt 2> ${LOGDIR}/${FN_BASE}_merge_err.txt
     echo "Cleaning up..."
     rm -f "$F.lwi"
     # rm -f script.avs
